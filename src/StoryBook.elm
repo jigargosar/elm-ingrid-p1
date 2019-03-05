f5dce9e8b2f44a3c3d1b4ceb1fe8400548fa8f2e
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-05 13:49:12 +0530

    story book

--- src/StoryBook.elm
+++ src/StoryBook.elm
@@ -76,11 +76,18 @@ view model =
 
 
 itemLabelStories =
+    let
+        defaultP =
+            { text = "I am default vanilla Item Label", isSelected = False, isRoot = False }
+
+        selectedP =
+            { defaultP | isSelected = True }
+    in
     Story.of_ "ItemLabel"
         [ Story.add "with defaultProps" <|
-            viewItemLabel { text = "I am default vanilla Item Label", isSelected = False, isRoot = False }
+            viewItemLabel defaultP
         , Story.add "with selected" <|
-            viewItemLabel { text = "I should be Selected", isSelected = True, isRoot = False }
+            viewItemLabel { selectedP | text = "I should be Selected" }
         , Story.add "with another defaultProps example" <|
-            viewItemLabel { text = "I am another plain label", isSelected = False, isRoot = False }
+            viewItemLabel { defaultP | text = "I am another plain label" }
         ]
