7746cf34131f1719c0d24a4962719416df281e6f
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-12 19:43:45 +0530

    custom tree

--- src/CTreeZipper.elm
+++ src/CTreeZipper.elm
@@ -11,14 +11,26 @@ type alias Crumb d =
     }
 
 
+type alias TreePivot d =
+    Pivot (Tree d)
+
+
 type alias ZipperModel d =
-    { pivot : Pivot d
-    , crumbs : List (Pivot d)
+    { pivot : TreePivot d
+    , crumbs : List (TreePivot d)
     }
 
 
 type Zipper d
     = Zipper (ZipperModel d)
+
+
+wrap =
+    Zipper
+
+
+unwrap (Zipper zm) =
+    zm
 
 
 fromTree : Tree d -> Zipper d
@@ -32,3 +44,9 @@ fromTree t =
 tree : Zipper d -> Tree d
 tree (Zipper zm) =
     Pivot.getC zm.pivot
+
+
+appendGoR : Tree d -> Zipper d -> Zipper d
+appendGoR t (Zipper zm) =
+    { zm | pivot = Pivot.appendGoR t zm.pivot }
+        |> wrap
